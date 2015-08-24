73534ed1843b140d5eed3112b4d75fb99680199a
Tom.Bonnike <tom.bonnike@hetic.net>
2015-08-24 17:20:48 +0200

    v0.1

--- assets/js/prod.js
+++ assets/js/prod.js
@@ -42,37 +42,37 @@ var Tracking = (function(_, tracking, document){
 		FastTracker 	= function(){
 			FastTracker.base(this, 'constructor');
 
-			this.corners   	 		   = [];   // Point d'intérêts
-			this.descriptors 		   = [];   // Descripteurs de ces points d'intérêts, utilisés pour faire le match
-			this.translationIntensity  = 0;    // Enregistrement de l'intensité (la hauteur) de translation de l'image
-			this.blur 	   			   = 4;    // Taux de flou à appliquer sur l'image (suppression des points d'intérêts peu précis)
-			this.uncertainty		   = 3;    // Hauteur d'incertitude en pixels pour valider le déplacement d'un point
-			this.translationThreshold  = 1;    // Hauteur en pixel nécessaire pour détecter qu'un mouvement est une translation
-			this.translationPercentage = 0.98; // Pourcentages des points d'intérêts qui doivent avoir bougé de la même hauteur pour que ce soit une translation d'image
+			this.corners   	 		   = [];   // Feature points
+			this.descriptors 		   = [];   // Feature points descriptors, used for the match
+			this.translationIntensity  = 0;    // Intensity (height) of the image translation
+			this.blur 	   			   = 4;    // Blur level
+			this.uncertainty		   = 3;    // Uncertainty level in pixels to validate the movement of a feature point
+			this.translationThreshold  = 1;    // Height in pixel that is necessary to detect that a movement was a translation
+			this.translationPercentage = 0.98; // Percentage of the feature points that have to have moved of the same height to detect an image translation
 			
-			this.bouncing		   		   = false;	// Booléen pour savoir si l'image est en train d'osciller
-			this.bounceIntensities 		   = [];	// Tableau qui enregistre les intensités de bounce à chaque translation d'image
-			this.lastBouncesLength 		   = 0;		// Enregistrement de la taille du tableau bounceIntensities
-			this.startTracking	   		   = false;	// Booléen pour savoir si on est prêt tracker une nouvelle translation d'image
-			this.startTrackingIndex 	   = 0;		// On enregistre à partir de quel index on peut rechercher ces nouveaux knock dans this.bounceIntensities
-			this.increasingBounceThreshold = 3; 	// Nombre de rebonds devant être supérieurs au précédent pour décider qu'un nouveau knock a eu lieu
-			this.interval 				   = null;	// Initialisation pour setInterval
+			this.bouncing		   		   = false;	// Bool that tells us if the image is bouncing
+			this.bounceIntensities 		   = [];	// Saved bouncing intensities of the last image translation
+			this.lastBouncesLength 		   = 0;		// The size of bounceIntensities is saved to know if new bounces have occured
+			this.startTracking	   		   = false;	// Bool that tells us if we can track a new image translation
+			this.startTrackingIndex 	   = 0;		// We save at which point we can search for new knocks in bounceIntensities
+			this.increasingBounceThreshold = 3; 	// Number of these bounces that have to be superior to the bounce-1 so that we can tell if a new knock occured
+			this.interval 				   = null;	// setInterval initialisation
 		};
 
 	tracking.inherits(FastTracker, tracking.Tracker);
 
 	FastTracker.prototype.isTranslation = function(matches){
-		if (matches.length > 1){ // Si il y a plus d'un match
+		if (matches.length > 1){ // If there's at least 1 match
 			var reliableKeypoint  = _.max(matches, function(match){return match.confidence;}),
-				translationHeight = reliableKeypoint.keypoint2[1] - reliableKeypoint.keypoint1[1]; // Hauteur de la translation du keypoint avec la meilleure confidence
+				translationHeight = reliableKeypoint.keypoint2[1] - reliableKeypoint.keypoint1[1]; // Height of the movement of the feature point with the highest confidence
 
-			this.translationIntensity = translationHeight; // On enregistre l'intensité de la translation d'image
+			this.translationIntensity = translationHeight; // We save the intensity of the movemnt
 			translationHeight 		  = Math.abs(translationHeight);
 
-			if (translationHeight >= this.translationThreshold) { // Si l'intensité de la translation est supérieure ou égale à notre seuil de translation (1px)
+			if (translationHeight >= this.translationThreshold) { // If the intensity is superior or egal to the threshold
 				/* 
-				Si plus de this.translationPercentage pourcent des points d'intérêts ont bougé de la même hauteur (en tenant compte de this.uncertainty)
-				c'est qu'il y a bien eu translation d'image 
+				If more than this.translationPercentage percent of the feature points moved of the same height (while taking into account this.uncertainty),
+				then an image translation occured and it wasn't just a movement.
 				*/
 				return _.filter(matches, function(match){
 					var difference = Math.abs(match.keypoint2[1] - match.keypoint1[1]);
@@ -92,16 +92,16 @@ var Tracking = (function(_, tracking, document){
 	};
 
 	FastTracker.prototype.checkNewBounce = function(){
-		// Si depuis la dernière exécution de la fonction (intervalle de 300ms), aucun nouveau rebond n'a eu lieu
+		// If since the last execution of that function (300ms), no new bounces occured
 		if (this.bounceIntensities.length == this.lastBouncesLength){
 			console.log('                             reset');
-			// On reset les valeurs et on attends la prochaine translation d'image
+			// We can reset all the values and wait for a new image translation
 			this.resetValues();
 			this.bouncing 		   = false;
 			this.bounceIntensities = [];
 		}
 
-		else { // Sinon, on mets à jour le nombre de rebonds
+		else { // Else, we update the number of bounces
 			this.lastBouncesLength = this.bounceIntensities.length;
 		}
 	};
@@ -115,21 +115,14 @@ var Tracking = (function(_, tracking, document){
 
 	FastTracker.prototype.initiateTimeout = function(that, timer){ // Feedback timer
 		setTimeout(function(){
-		/*	if (that.bounceIntensities.length){ // Si on a eu des rebonds après avoir lancé le timeout*/
-				console.log('Knock Knock, intensity ' + Math.abs(_.max(that.bounceIntensities))); // On enregistre l'intensité en prenant le rebond le plus important
-				that.knock(); // On lance le feedback
+				that.knock(); // Feedback (that should take the highest intensity level that occured 100ms after the knock)
 
-				setTimeout(function(){ // 50ms plus tard, on initialise des variables utilisées pour détecter si un nouveau rebond a eu lieu
+				setTimeout(function(){ // 50ms later, we initialise the variables used to detect if a new knock occured or if the movement are oscillations of the last one
 					that.startTracking 		= true;
 					that.startTrackingIndex = that.bounceIntensities.length - 1;
 				}, 50);
 
-				that.interval = setInterval(function(){that.checkNewBounce();}, 300); // On lance l'interval pour savoir si les rebonds se sont arrêtés
-		/*	}
-
-			else { // Sinon, c'est que les rebonds n'étaient pas assez importants pour être enregistrés
-				that.bouncing = false;
-			}*/
+				that.interval = setInterval(function(){that.checkNewBounce();}, 300); // We launch the interval used to know if the bounces stopped.
 		}, timer);
 	};
 
@@ -140,33 +133,34 @@ var Tracking = (function(_, tracking, document){
 			descriptors    = tracking.Brief.getDescriptors(gray, width, corners),
 			matchedCorners = tracking.Brief.reciprocalMatch(this.corners, this.descriptors, corners, descriptors);
 
-		// Filtrage des matchs avec une confidence inférieure à 0.75 ou avec une valeur de -Infinity/Infinity (?)
+		// Filtering the matches with a confidence < .75 or with a value of -Infinity/Infinity (?)
 		matchedCorners = _.filter(matchedCorners, function(match){return match.confidence >= 0.75 && match.confidence !== Math.abs(Infinity);}); 
 
-		if (this.isTranslation(matchedCorners)){ // Si il y a eu une translation sur cette frame
-			if (!this.bouncing){ // Si cette translation n'est pas un des rebonds d'un autre knock
+		if (this.isTranslation(matchedCorners)){ // If a translation happened on this image frame
+			if (!this.bouncing){ // If that translation isn't just a bounce of another knock
 				this.bouncing = true;
-				this.initiateTimeout(this, 100); // On lance le timer de feedback
+				this.initiateTimeout(this, 100); // We launch the feedback timer
 			}
 
 			else {
 				var bounceIntensities = this.bounceIntensities,
-					positiveBounces   = _.filter(bounceIntensities.slice(this.startTrackingIndex), function(bounce){return bounce > 0;}), // On ne prends que les bonds d'intensité positive après le début du track
-					lastBounces 	  = _.last(positiveBounces, this.increasingBounceThreshold); // On prends les this.increasingBounceThreshold dernière valeures de bounceIntensities
+					positiveBounces   = _.filter(bounceIntensities.slice(this.startTrackingIndex), function(bounce){return bounce > 0;}), // We only take the bounces with a positive intensity after we started tracking for a new knock
+					lastBounces 	  = _.last(positiveBounces, this.increasingBounceThreshold); // We test on this.increasingBounceThreshold values of bounceIntensities
 
-				bounceIntensities.push(this.translationIntensity); // On ajoute le nouveau rebond au tableau bounceIntensities
+				bounceIntensities.push(this.translationIntensity); // We add the new bounce to the bounceIntensities array
 
 				if (this.startTracking && lastBounces.length && _.every(lastBounces, function(bounce){return bounce > positiveBounces[positiveBounces.length - this.increasingBounceThreshold - 1];}, this)){
-					/* Si tous les this.increasingBounceThreshold derniers rebonds sont d'une intensité supérieure à l'index this.increasingBounceThreshold
-					C'est que c'est un nouveau knock */
-					this.resetValues(); // On reset les valeurs
-					this.bounceIntensities = [_.last(bounceIntensities)]; // La dernière valeur du tableau actuel devient la première et le reste des valeurs sont supprimées
-					this.initiateTimeout(this, 50); // On relance le timer de feedback
+					/* If the this.increasingBounceThreshold last values were higher than their bounce-1, 
+					   it was a new knock and not only the oscillation because of the last bounce.
+					*/
+					this.resetValues(); // We reset all values used for the tests
+					this.bounceIntensities = [_.last(bounceIntensities)]; // The last value of the actual bounceIntensities array becomes the first of the new array, and the old values are deleted
+					this.initiateTimeout(this, 50); // We launch the feedback timer again
 				}
 			}
 		}
 
-		// Les corners et descriptors de la nouvelle frame sont enregistrés et remplacent donc les valeurs de l'ancienne frame
+		// The corners and descriptors of the new frame are saved for the next frame testing
 		this.corners 	 = corners;
 		this.descriptors = descriptors;
 
