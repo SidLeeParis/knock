e2f1ec3b74ca556f728157336cab15e94aa23f49
Tom.Bonnike <tom.bonnike@hetic.net>
2015-08-27 12:31:31 +0200

    v0.1

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var express = require('express'),
-	path 	= require('path');
-
+var express	   = require('express'),
+	path 	   = require('path');
+	
 app = express();
 
-app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 5000;
