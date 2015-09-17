b131de9b9e9e21150c67643982e875b88343fda2
David Bismut <david.bismut@gmail.com>
2015-09-17 15:17:47 +0200

    Speed varies depending on intensity

--- src/assets/js/modules/sidlee.js
+++ src/assets/js/modules/sidlee.js
@@ -35,6 +35,7 @@ Modules.sidlee = function(){
 		};
 
 		var explodingRadius = Math.min(_canvas.width * 2 / 5, _canvas.width * intensity / 20);
+		_totalIterations =  Math.min(Math.floor(30 / intensity * 10), 80);
 
 		for(i = 0; i < _particles.length; i++) {
 			particle = _particles[i];
